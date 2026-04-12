dd2e9261e660e6c725371bfca982a051c01d995f
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-12 18:10:08 +0300

    feat: implement insertion sort algorithm

---
+++ InsertionSort/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {8, 3, 7, 1, 9, 2, 6, 5, 4, 0, 0};
+
+        insertionSort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
