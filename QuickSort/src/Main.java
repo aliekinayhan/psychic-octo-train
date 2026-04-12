a914d3e234329952b0a469f78d06841268fad9a1
aliekinayhan <ayhanaliekin0@gmail.com>
2026-04-12 21:03:53 +0300

    feat: implement quick sort algorithm

---
+++ QuickSort/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {8, 3, 7, 1, 9, 2, 6, 5, 4, 0};
+        System.out.println(Arrays.toString(numbers));
+        quickSort(numbers, 0, numbers.length - 1);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
